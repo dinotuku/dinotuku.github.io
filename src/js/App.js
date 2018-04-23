068b26aa993559b9a4c301d76087871429b461d1
dinotuku <kuan.tung@epfl.ch>
2018-04-23 21:32:59 +0800

    Change BrowserRouter to Hash Router

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Main from './components/Main';
@@ -11,7 +11,7 @@ import '../css/index.css';
 class App extends Component {
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <div className="App">
           <Header />
           <Route path="/" exact component={Main}></Route>
