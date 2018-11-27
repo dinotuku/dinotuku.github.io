f82335e055e2f2e8e02b8da55fe4a77346208d20
dinotuku <tungkuan0528@gmail.com>
2018-11-27 12:52:16 +0800

    Update resume (remove Chinese)

--- src/js/components/Resume.js
+++ src/js/components/Resume.js
@@ -13,28 +13,15 @@ const Resume = () => (
         <Row>
           <Col>
             <Jumbotron className="bg-white d-flex flex-wrap justify-content-around align-items-center">
-              <h1 className="display-4">English</h1>
+              <div className="my-3">
+                <h1 className="display-4">Resume</h1>
+                <p className="lead">About me</p>
+              </div>
               <div className="iframe-wrapper">
                 <div className="fixed-aspect-ratio">
-                  <iframe className="ml-aute" title="eng_resume" src="https://gdurl.com/AJ57/viewer" width="500" height="100%" frameBorder="0"></iframe>
+                  <iframe className="ml-aute" title="resume" src="https://gdurl.com/KA4X/viewer" width="500" height="100%" frameBorder="0"></iframe>
                 </div>
               </div>
-            </Jumbotron>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-    <section className="full-height bg-light">
-      <Container className="bg-light">
-        <Row>
-          <Col>
-            <Jumbotron className="bg-light d-flex flex-wrap-reverse justify-content-around align-items-center">
-              <div className="iframe-wrapper">
-                <div className="fixed-aspect-ratio">
-                  <iframe className="ml-aute" title="chi_resume" src="https://gdurl.com/705MF/viewer" width="500" height="700" frameBorder="0"></iframe>
-                </div>
-              </div>
-              <h1 className="display-4 chi">中文</h1>
             </Jumbotron>
           </Col>
         </Row>
