9a6e4751f9b0c13c10edbb74bada871c7218b180
dinotuku <tungkuan0528@gmail.com>
2019-09-11 18:45:58 +0200

    Updates

---
+++ precache-manifest.ca065eefa13cefc276a4b7f9e30a9882.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1e57194e617149b0ffb1d63f859d3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e297745430e07f7f7d82",
+    "url": "/static/css/main.dd7e968f.chunk.css"
+  },
+  {
+    "revision": "a90a7f6e86057caca748",
+    "url": "/static/js/2.a7c704f5.chunk.js"
+  },
+  {
+    "revision": "e297745430e07f7f7d82",
+    "url": "/static/js/main.72425e3d.chunk.js"
+  },
+  {
+    "revision": "9c32cdf71121e1d25628",
+    "url": "/static/js/runtime~main.31236ffa.js"
+  },
+  {
+    "revision": "dae4e40fcfdef4c75931886e51226a39",
+    "url": "/static/media/logo.dae4e40f.svg"
+  },
+  {
+    "revision": "680fe8d4cd55da87019268804032bc6f",
+    "url": "/static/media/profile.680fe8d4.jpg"
+  }
+]);
